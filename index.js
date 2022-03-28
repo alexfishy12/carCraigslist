b424527a64ac4960e97dde21345c1511cdd31068
alexfishy12 <58009009+alexfishy12@users.noreply.github.com>
2022-03-28 11:09:48 -0400

    Added base files for website.

---
+++ index.js
@@ -0,0 +1,29 @@
+//set global variables
+var username;
+
+//when html is done loading
+$(document).ready(function(){
+
+})
+
+//load menu of cars
+function loadCars(){
+    
+}
+
+//use function to get cookie from browser example: getCookie("username") will return stored username if cookie was set during login
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i <ca.length; i++) {
+      	let c = ca[i];
+      	while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+    return "";
+}
